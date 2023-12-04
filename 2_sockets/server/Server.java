b69be1d3783773a35048552c3dbd9d8728fedb1b
anwazmi <anwarmazmi@gmail.com>
2023-12-05 07:17:53 +0800

    trial

--- 2_sockets/server/Server.java
+++ 2_sockets/server/Server.java
@@ -1,79 +1,129 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.net.*;
+import java.util.*;
 
-public class Server {
+class Server {
 
-    private static final String ip = "127.0.0.1";
-    private static final int port = 12345;
+  private ServerSocket server;
+  private Map<String, ClientHandler> clients;
+  private ArrayList<String> fileNames;
+  
+  public Server(int port) throws IOException {
+    server = new ServerSocket(port);
+    clients = new HashMap<>();
+    fileNames = new ArrayList<>();
+  }
 
-    private static void join() {
-        try (Socket clientSocket = new Socket(ip, port)) {
-            System.out.println("Connected to the File Exchange Server at " + ip + ":" + port + " successfully!");
-        } catch (IOException e) {
-            System.err.println("Error: Connection to the Server has failed! Please check IP Address and Port Number.");
+  public void start() throws IOException {
+    while(true) {
+      Socket client = server.accept();
+      BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      String handle = in.readLine();
+      if(handle == null) continue;
+      handle = handle.substring(1);
+      ClientHandler handler = new ClientHandler(client, handle);
+      clients.put(handle, handler);
+      handler.start();
+    }
+  }
+
+  private void broadcast(String message) {
+    for(ClientHandler client : clients.values()) {
+      client.sendMessage(message);
+    }
+  }
+  
+  private class ClientHandler extends Thread {
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private String handle;
+
+    public ClientHandler(Socket client, String handle) throws IOException {
+      this.client = client;
+      this.handle = handle;
+      in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      out = new PrintWriter(client.getOutputStream(), true);
+      sendMessage("Registration successful. Welcome " + handle);
+    }
+
+    public void run() {
+      try {
+        while(true) {
+          String request = in.readLine();
+          if(request.startsWith("/store")) {
+            handleStore(request);
+          }
+          else if(request.equals("/dir")) {
+            handleDir();  
+          }
+          else if(request.startsWith("/get")) {
+            handleGet(request);
+          }
+          else if(request.equals("/leave")) {
+            break;
+          }
+          else {
+            sendError("Invalid command");
+          }
         }
+      } catch (IOException e) {
+        // Client disconnected
+      } finally {
+        clients.remove(handle);
+        broadcast(handle + " has left");
+      }
     }
 
-    private static void leave() {
-        System.out.println("Connection closed. Thank you!");
+    private void handleStore(String request) {
+      String[] parts = request.split(" ");
+      if(parts.length != 2) {
+        sendError("Invalid parameters");
+        return;
+      }
+
+      String fileName = parts[1];
+      if(!new File(fileName).exists()) {
+        sendError("File not found");
+        return;
+      }
+
+      fileNames.add(fileName);
+      broadcast(handle + " stored " + fileName);
     }
 
-    private static void register(String handle) {
-        if (handle != null && !handle.isEmpty()) {
-            System.out.println("Welcome " + handle + "!");
-        } else {
-            System.err.println("Error: Registration failed. Handle or alias cannot be empty.");
-        }
+    private void handleDir() {
+      String fileList = String.join(",", fileNames);
+      sendMessage("Files on server: " + fileList);
     }
 
-    private static void store(String filename, String handle) {
-        if (filename != null && !filename.isEmpty()) {
-            String timestamp = getCurrentTimestamp();
-            System.out.println(handle + "<" + timestamp + ">: Uploaded " + filename);
-        } else {
-            System.err.println("Error: File name cannot be empty.");
-        }
+    private void handleGet(String request) {
+     String[] parts = request.split(" ");
+      if(parts.length != 2) {
+        sendError("Invalid parameters");
+        return;  
+      }
+      String fileName = parts[1];
+      if(!fileNames.contains(fileName)) {
+        sendError("File not found");
+        return;
+      }
     }
 
-    private static void dir() {
-        System.out.println("Server Directory\nHello.txt\nIMG001.bmp");
+    public void sendMessage(String msg) {
+      out.println(msg); 
+    }
+    
+    public void sendError(String err) {
+      out.println("Error: " + err);
     }
 
-    private static void get(String filename, String handle) {
-        if (filename != null && !filename.isEmpty()) {
-            System.out.println("File received from Server: " + filename);
-        } else {
-            System.err.println("Error: File name cannot be empty.");
-        }
-    }
+  }
 
-    private static String getCurrentTimestamp() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return dateFormat.format(new Date());
-    }
+  public static void main(String[] args) throws IOException {
+    Server server = new Server(12345);
+    server.start(); 
+  }
 
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("The server is ready: Port " + port);
-
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (serverSocket != null && !serverSocket.isClosed()) {
-                    serverSocket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
